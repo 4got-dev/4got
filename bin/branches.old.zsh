7b6a7bf3d85d4b6f7cf25e17ec9bf115de178002
fogrew <andrew@gurylev.com>
2022-02-16 00:32:06 +0300

    docs(branches.old): add shdocs

--- bin/branches.old.zsh
+++ bin/branches.old.zsh
@@ -1,7 +1,17 @@
 
-# TODO: add docs
+# 4got.branches.old
+# 
+# @description Show branches sorted by committer date. By default last 5 branches, but you can pass number argument to change this value
+#
 # @arg $1 number How much old branches to show
-# TODO: add error codes
+#
+# @example
+#     4got.branches.old
+# @example
+#     4got.branches.old 10
+#
+# @exitcode 0 If successful
+# @exitcode 1 If git returns error
 function 4got.branches.old() {
   if [ $# -eq 0 ]; then
     local refnames=5
@@ -11,4 +21,3 @@ function 4got.branches.old() {
 
   git for-each-ref --sort=committerdate --color --format='No updates to %(color:bold green)%(refname:short)%(color:reset) since %(color:bold blue)%(committerdate:short)%(color:reset)' | head -n $refnames
 }
-# TODO: add compdef
