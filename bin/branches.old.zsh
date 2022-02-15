cf474421929acabea58f1128a709950d4e796e5a
fogrew <andrew@gurylev.com>
2022-02-16 00:31:10 +0300

    fix(branches.old): add colors to output, split if

--- bin/branches.old.zsh
+++ bin/branches.old.zsh
@@ -3,11 +3,12 @@
 # @arg $1 number How much old branches to show
 # TODO: add error codes
 function 4got.branches.old() {
-  # TODO: split into lines
-  if [[ $# -eq 0 ]]; then; local hed=10; else; local hed=$1; fi
-  echo $hed # TODO: what for? maybe specify more info?
-  echo $#
-  # TODO: add coloring
-  git for-each-ref --sort=committerdate --format='No updates to %(refname:short) since %(committerdate:short)...' | head -n $hed
+  if [ $# -eq 0 ]; then
+    local refnames=5
+  else
+    local refnames=$1
+  fi
+
+  git for-each-ref --sort=committerdate --color --format='No updates to %(color:bold green)%(refname:short)%(color:reset) since %(color:bold blue)%(committerdate:short)%(color:reset)' | head -n $refnames
 }
 # TODO: add compdef
