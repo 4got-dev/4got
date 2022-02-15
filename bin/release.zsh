fad96ca36fd282b9a5f6525cc50dcbc1cdedd9a5
fogrew <andrew@gurylev.com>
2022-02-16 00:26:32 +0300

    fix(release): format codestyle

--- bin/release.zsh
+++ bin/release.zsh
@@ -1,7 +1,13 @@
 # TODO: add docs
 function 4got.release() {
   # TODO: split into lines
-  if [[ $# -eq 0 ]]; then; echo "You must pass a tag to release."; else; 4got.update master && git tag $1 && git push origin $1; fi;
+  if [ $# -eq 0 ]; then
+    echo "You must pass a tag to release."
+  else
+    4got.update master
+    git tag $1
+    git push origin $1
+  fi
 }
 # TODO: add compdef
 # TODO: make github/gitlab/npm release via push options
